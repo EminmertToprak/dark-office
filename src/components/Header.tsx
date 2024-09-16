da8118a8df650ff9aa05161857a5b61dbd61632a
Mert Toprak <mert.tprk@gmail.com>
2024-09-16 15:24:55 +0200

    fixed image type

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ import { useEffect, useState } from 'react';
 interface HeaderProps {
 	title: string;
 	subtitle: string;
-	backgroundImage: any;
+	backgroundImage: string;
 	backgroundVideo?: string;
 }
 
